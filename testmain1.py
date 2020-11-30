35a5d3f6739455a948869131cd2df369eaca790e
Nguyễn Xuân Cừ <48949007+cuwfnguyen@users.noreply.github.com>
2020-11-30 04:50:22 +0000

    Add files via upload

--- testmain1.py
+++ testmain1.py
@@ -3,14 +3,13 @@ import json
 import serial
 s=serial.Serial('/dev/ttyUSB0',9600)
 
-host = "broker.hivemq.com"
+host = "mqtt.eclipse.org"
 port = 1883
 interval = 600
 topic1="IoT_BT1"
 topic2="IoT_BT2"
 
 def receive_arduino():
-    valueLed=1
     value1=s.readline()
     value2=s.readline()
     value3=s.readline()
@@ -18,7 +17,7 @@ def receive_arduino():
     valueG=value1[2:5]
     valueT=value2[2:4]
     valueH=value3[2:4]
-    mes=json.dumps({"led":valueLed,"gas":int(valueG.decode()), "temperature":int(valueT.decode()),"humidity":int(valueH.decode())});
+    mes=json.dumps({"gas":int(valueG.decode()), "temperature":int(valueT.decode()),"humidity":int(valueH.decode())});
     return mes
 #def send_arduino():
     
@@ -29,14 +28,15 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):  
     #print("Message :" + msg.topic + " " + str(msg.payload))  
-    if msg.topic=="IoT_BT2":
-        #print(msg.payload)
-        if int(msg.payload) == 1 :
-           # print("1\n")
-            s.write("1\n".encode())
-        if int(msg.payload) == 0:
-            s.write("0\n".encode())
-          #  print("0\n")
+    if msg.topic=="IoT_BT1":
+       # print(msg.payload)
+        data=json.loads(msg.payload)
+        if int(data["led1"]) == 1 :
+            #print("1")
+            s.write("led1ON\n".encode())
+        if int(data["led1"]) == 0 :
+            s.write("led1OFF\n".encode())
+          #  print("0")
    # if msg.topic=="IoT_BT2":
     #    led2=msg.payload
     #    s.write(led2)
